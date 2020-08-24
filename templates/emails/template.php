6671a22e33f7109ea5a2efb73f37a317dd5d1a08
Tien Nguyen <mrtienhp97@gmail.com>
2020-08-24 17:42:55 +0700

    change bot link

--- templates/emails/template.php
+++ templates/emails/template.php
@@ -9,7 +9,7 @@ $telegram_chatbot_code = get_post_meta( $order->get_id(), 'telegram_chatbot_code
 ?>
 
 	<p>Chúc mừng bạn đã đăng ký thành công Mayor Capital Membership.</p>
-    <p>Bot Username: <a href="https://web.telegram.org/#/im?p=@bonbonpocbot">@bonbonpocbot</a></p>
+    <p>Bot Username: <a href="https://t.me/bonbonpocbot">@bonbonpocbot</a></p>
 	<p>Mã code truy cập: <strong><?php echo $telegram_chatbot_code; ?></strong></p>
 	<p>Xem hướng dẫn tham gia group tại đây: <a href="https://mayor.capital/huongdan">https://mayor.capital/huongdan</a></p>
 
