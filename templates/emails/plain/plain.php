0fe60356d92404f923ecda8d2ad71d229b0ff8df
Tien Nguyen <mrtienhp97@gmail.com>
2020-08-24 16:14:17 +0700

    update bot username

--- templates/emails/plain/plain.php
+++ templates/emails/plain/plain.php
@@ -6,7 +6,7 @@ $telegram_chatbot_code = get_post_meta( $order->ID, 'telegram_chatbot_code' );
 
 <p>Chúc mừng bạn đã đăng ký thành công Mayor Capital Membership.</p>
 
-<p>Link Bot: <a href="https://web.telegram.org/#/im?p=@BonBonVN_bot">https://web.telegram.org/#/im?p=@BonBonVN_bot</a></p>
+<p>Bot Username: <a href="https://web.telegram.org/#/im?p=@bonbonpocbot">@bonbonpocbot</a></p>
 
 <p>Mã code truy cập của bạn: <strong><?php echo $telegram_chatbot_code; ?></strong></p>
 
--- templates/emails/template.php
+++ templates/emails/template.php
@@ -9,8 +9,8 @@ $telegram_chatbot_code = get_post_meta( $order->get_id(), 'telegram_chatbot_code
 ?>
 
 	<p>Chúc mừng bạn đã đăng ký thành công Mayor Capital Membership.</p>
-    <p>Link Bot: <a href="https://web.telegram.org/#/im?p=@BonBonVN_bot">https://web.telegram.org/#/im?p=@BonBonVN_bot</a></p>
-	<p>Mã code truy cập của bạn: <strong><?php echo $telegram_chatbot_code; ?></strong></p>
+    <p>Bot Username: <a href="https://web.telegram.org/#/im?p=@bonbonpocbot">@bonbonpocbot</a></p>
+	<p>Mã code truy cập: <strong><?php echo $telegram_chatbot_code; ?></strong></p>
 	<p>Xem hướng dẫn tham gia group tại đây: <a href="https://mayor.capital/huongdan">https://mayor.capital/huongdan</a></p>
 
 <?php
